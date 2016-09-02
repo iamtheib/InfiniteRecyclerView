6d9fa8563b131a344a4ca95e5b685ada179b1ea5
Ibrahim Sikiru <ib@iamtheib.me>
2016-09-02 00:44:07 -0400

    move load more logic into the adapter
    The adapter uses the position of the view in the
    onBindViewHolder method to calculate when to trigger
    the load more callback. No more onScrollListener
    
    After moving this logic from the custom RecyclerView,
    it became pretty much useless. So it has been completely
    removed, in favor of just extending the adapter, and using
    it with any recycler view
    
    Breaking changes:
    
        - InfiniteRecyclerView is gone. Using the normal
          RecylerView, or any other extension of it, in your
          layout, should work fine.
        - All the methods previously called on the RecyclerView
          in the Activity/Fragment, should now be called on the
          adapter
        - Removed deprecated moreDataLoaded()

--- library/src/main/java/com/sa90/infiniterecyclerview/InfiniteAdapter.java
+++ library/src/main/java/com/sa90/infiniterecyclerview/InfiniteAdapter.java
@@ -3,22 +3,56 @@ package com.sa90.infiniterecyclerview;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import com.sa90.infiniterecyclerview.listener.OnLoadMoreListener;
+
 /**
  * Created by Saurabh on 6/2/16.
+ *
+ * This supports a callback to notify when to load more items.
+ *
+ * Implementing Activities/fragments should also call moreDataLoaded(int, int)
+ * to inform the adapter that more data has been loaded.
  */
 public abstract class InfiniteAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter {
 
     private static final int VIEW_TYPE_LOADING = 0;
-    private boolean shouldLoadMore = true;
+
+    private boolean mShouldLoadMore = true;
+    private boolean mIsLoading = false;
     // Used to indicate the infinite scrolling should be bottom up
-    private boolean isReversedScrolling = false;
+    private boolean mIsReversedScrolling = false;
+    private OnLoadMoreListener mLoadMoreListener;
 
     @Override
     public final RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if(viewType == VIEW_TYPE_LOADING)
+        if(viewType == VIEW_TYPE_LOADING) {
             return getLoadingViewHolder(parent);
-        else
+        } else {
             return onCreateView(parent, viewType);
+        }
+    }
+
+    /**
+     * Subclasses should override this method, to actually bind the view data
+     * but always call <code>super.onBindViewHolder(ViewHolder, int)</code> to
+     * enable the adapter to know when to actually load more
+     *
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (mShouldLoadMore && !mIsLoading) {
+            int threshold = getVisibleThreshold();
+            boolean hasReachedThreshold = mIsReversedScrolling ? position <= threshold
+                                                               : position >= getCount() - threshold;
+            if (hasReachedThreshold) {
+                mIsLoading = true;
+                if (mLoadMoreListener != null) {
+                    mLoadMoreListener.onLoadMore();
+                }
+            }
+        }
     }
 
     @Override
@@ -28,37 +62,71 @@ public abstract class InfiniteAdapter<VH extends RecyclerView.ViewHolder> extend
         // not affect the item count.
         // The consequence of this is, while there is more data to load, the first item on
         // the list will be replaced by the loading view
-        if(actualCount == 0 || !shouldLoadMore || isReversedScrolling)
+        if(actualCount == 0 || !mShouldLoadMore || mIsReversedScrolling) {
             return actualCount;
-        else
+        } else {
             return actualCount + 1;
+        }
     }
 
     @Override
     public final int getItemViewType(int position) {
-        if(isLoadingView(position))
+        if(isLoadingView(position)) {
             return VIEW_TYPE_LOADING;
-        else {
+        } else {
             int viewType = getViewType(position);
-            if(viewType == VIEW_TYPE_LOADING)
+            if (viewType == VIEW_TYPE_LOADING) {
                 throw new IndexOutOfBoundsException("0 index is reserved for the loading view");
-            else
+            } else {
                 return viewType;
+            }
         }
     }
 
     private boolean isLoadingView(int position) {
         // For reversed scrolling, the loading view is always the top one
-        int loadingViewPosition = isReversedScrolling ? 0 : getCount();
-        return position == loadingViewPosition && shouldLoadMore;
+        int loadingViewPosition = mIsReversedScrolling ? 0 : getCount();
+        return position == loadingViewPosition && mShouldLoadMore;
     }
 
     public void setShouldLoadMore(boolean shouldLoadMore) {
-        this.shouldLoadMore = shouldLoadMore;
+        this.mShouldLoadMore = shouldLoadMore;
     }
 
     public void setIsReversedScrolling(boolean reversed) {
-        this.isReversedScrolling = reversed;
+        this.mIsReversedScrolling = reversed;
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener listener) {
+        this.mLoadMoreListener = listener;
+    }
+
+    /**
+     * This informs the adapter that <code>itemCount</code> more data has been loaded,
+     * starting from <code>positionStart</code>
+     *
+     * This also calls notifyItemRangeInserted(int, int),
+     * so the implementing class only needs to call this method
+     *
+     * @param positionStart Position of the first item that was inserted
+     * @param itemCount Number of items inserted
+     *
+     */
+    public void moreDataLoaded(int positionStart, int itemCount) {
+        mIsLoading = false;
+        notifyItemRangeInserted(positionStart, itemCount);
+    }
+
+    /**
+     * Returns the number of scrollable items left (threshold) in the list before
+     * <code>OnLoadMoreListener</code> will be called
+     *
+     * You can override this to return the desired threshold
+     *
+     * @return integer threshold
+     */
+    public int getVisibleThreshold() {
+        return 5;
     }
 
     /**
